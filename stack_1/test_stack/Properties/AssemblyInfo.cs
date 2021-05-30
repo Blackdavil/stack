d738b7e08dfa8734b147653657da88113b04afba
karimov006 <reloaded_123@mail.ru>
2021-05-30 19:24:09 +0500

    stack_proj

---
+++ stack_1/test_stack/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test_stack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test_stack")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("32c4c502-4c42-4e21-9039-3719e509fe65")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ stack_1/test_stack/UnitTest1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using stack_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace stack_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void stack_campacity()
+        {
+            int campacity = 3;
+            Stack s = new Stack(campacity);
+
+            for (int i = 0; i < 5; i++)
+            {
+                s.Push("element" + i);
+            }
+
+            Assert.AreEqual(campacity, s.Count());
+        }
+        [TestMethod]
+        public void push_check()
+        {
+            int campacity = 1;
+            Stack s = new Stack(campacity);
+            s.Push("test");
+
+            Assert.AreEqual("test", s.Peek());
+
+        }
+        [TestMethod]
+        public void peek_test()
+        {
+            int campacity = 2;
+            Stack s = new Stack(campacity);
+            s.Push("test");
+
+            Assert.AreEqual("test", s.Peek());
+            s.Push("result");
+            Assert.AreEqual("result", s.Peek());
+
+
+        }
+        [TestMethod]
+        public void pop_test()
+        {
+            int campacity = 2;
+            Stack s = new Stack(campacity);
+            s.Push("test");
+
+            Assert.AreEqual("test", s.Peek());
+            s.Push("result");
+            Assert.AreEqual("result", s.Peek());
+            s.Pop();
+            Assert.AreEqual("test", s.Peek());
+            s.Pop();
+            Assert.AreEqual("Stack is Empty", s.Peek());
+
+        }
+        [TestMethod]
+        public void isEmpty_test()
+        {
+            int campacity = 2;
+            Stack s = new Stack(campacity);
+
+            Assert.IsTrue(s.isEmpty());
+            s.Push("NowIsNotEmpty");
+            Assert.IsFalse(s.isEmpty());
+
+        }
+        [TestMethod]
+        public void isFull_test()
+        {
+            int campacity = 2;
+            Stack s = new Stack(campacity);
+
+            Assert.IsFalse(s.isFull());
+            s.Push("NowIsNotEmpty");
+            Assert.IsFalse(s.isFull());
+            s.Push("Now is Full");
+            Assert.IsTrue(s.isFull());
+
+        }
+        [TestMethod]
+        public void clear_test()
+        {
+            int campacity = 10;
+            Stack s = new Stack(campacity);
+            for (int i = 0; i < 10; i++)
+            {
+                s.Push(i.ToString());
+            }
+
+            Assert.IsFalse(s.isEmpty());
+            s.Clear();
+            Assert.IsTrue(s.isEmpty());
+
+        }
+        [TestMethod]
+        public void Count_test()
+        {
+            int campacity = 10;
+            Stack s = new Stack(campacity);
+
+            Assert.AreEqual(0, s.Count());
+
+            for (int i = 0; i < 10; i++)
+            {
+                s.Push(i.ToString());
+            }
+
+            Assert.AreEqual(10, s.Count());
+            s.Pop();
+            Assert.AreEqual(9, s.Count());
+            s.Clear();
+
+            Assert.AreEqual(0, s.Count());
+
+        }
+
+
+
+    }
+}
